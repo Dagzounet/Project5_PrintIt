722307a1479d1dec4d9cc4e3a71fef25e6c294a6
Dagzounet <paul.gamot@gmail.com>
2023-06-08 19:40:57 +0200

    total change and update of javascript file to make a simpler and working script for the image slider
    total change and update of javascript file to make a simpler and working script for the image slider

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// ici je définis un tableau appelé "slides" qui contient les différentes images et leurs textes
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,23 +18,47 @@ const slides = [
 	}
 ]
 
-//variables pour chaque fleche que je recupere dans mon html :
+// Je récupère les éléments nécessaires dans mon HTML : ici la baniere et son texte, les fleches, les points.
+const banner = document.getElementById("banner");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot");
+const tagLine = document.querySelector("#banner p");
 
-const leftArrow = document.querySelector('.arrow_left');
-const rightArrow = document.querySelector('.arrow_right');
+let currentSlide = 0; // Je créer un index pour la diapositive actuelle
 
-//variables pour les points que je recupere dans mon html :
+// Création d'une fonction pour mettre à jour le diaporama :
+function updateSlide() {
 
-const dot = document.querySelector('.dot');
-const currentDot = document.querySelector('.dot_selected');
+	//creation constante pour les slides :
+  const slide = slides[currentSlide];
 
-//Events que j'assigne à mes variable, execution du contenu defini, ici console log, au clic :
+  // Mise à jour de l'image et de son texte :
+  document.querySelector(".banner-img").src = "./assets/images/slideshow/" + slide.image;
+  tagLine.innerHTML = slide.tagLine;
 
-leftArrow.addEventListener('click', () => {
-	console.log("fleche gauche clique");
+  // Mise à jour des points qui s'aligne sur l'index par un égal à currentSlide:
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("dot_selected", index === currentSlide);
+  });
+}
+
+// Événement clic sur la flèche de gauche (si la slide actuel est égale à 0, alors définir la variable à -1 pour la précédente, sinon retirer 1)
+arrowLeft.addEventListener("click", () => {
+  if (currentSlide === 0) { //si on est sur la première diapo et qu'on veut aller sur la dernière alors le -1 permet d'aller sur l'index de fin du tableau
+    currentSlide = slides.length - 1;
+  } else { //sinon, on soustrait 1 à la variable pour passer à la précédente.
+    currentSlide--;
+  }
+  updateSlide(); // lancement de la fonction précédement défini.
 });
 
-rightArrow.addEventListener('click', () => {
-	currentDot.classList.remove('dot_selected');
-	console.log("fleche droite clique");
+// Événement clic sur la flèche de droite (si la slide actuel est égale à -1, alors définir à 0 pour la suivante, sinon ajouter 1)
+arrowRight.addEventListener("click", () => {
+  if (currentSlide === slides.length - 1) { //idem ici si la dernière diapositive est affiché, pour pouvoir repasser à la 1ere la variable est défini sur 0
+    currentSlide = 0;
+  } else { //sinon on ajoute 1 à la variable pour passer à la suivante
+    currentSlide++;
+  }
+  updateSlide(); // lancement de la fonction précédement défini.
 });
